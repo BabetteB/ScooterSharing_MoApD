3cfff46efaa54c148672c8ea01916308fadbcd69
Babette Bækgaard <38117208+BabetteB@users.noreply.github.com>
2023-04-25 14:42:42 +0200

    OBS! Timer starts before scooter is unlocked! Needs fix! added layout and function to layout so that user can only start a ride if a scooter is selected, otherwise it will give you info that you need to select a scooter. Scooter has timer and price (needs to update price based on timer in the future). Needs to update progress bar so it counts down minutes, where price is updated maybe?

--- app/src/main/java/dk/itu/moapd/scootersharing/babb/viewmodel/ScooterFragment.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/babb/viewmodel/ScooterFragment.kt
@@ -5,22 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Chronometer
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import dk.itu.moapd.scootersharing.babb.R
 import dk.itu.moapd.scootersharing.babb.databinding.FragmentScooterBinding
 import dk.itu.moapd.scootersharing.babb.databinding.FragmentStartRideBinding
+import dk.itu.moapd.scootersharing.babb.model.Scooter
+import dk.itu.moapd.scootersharing.babb.model.ScooterViewModel
+import java.time.Duration
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ScooterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ScooterFragment : Fragment() {
+    private val vm : ScooterViewModel by activityViewModels()
 
     private var _binding : FragmentScooterBinding? = null
     private val binding
@@ -30,6 +28,7 @@ class ScooterFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -38,16 +37,57 @@ class ScooterFragment : Fragment() {
     ): View? {
         _binding = FragmentScooterBinding.inflate(layoutInflater, container, false)
 
+        with (binding) {
+            if (vm.getUserScooter() == null) {
+                scooterFragmentTitle.text = "No scooter ride in progress"
+
+                activeScooterName.text = "Please start a ride before information can be shown."
+                activeScooterPrice.visibility = View.INVISIBLE
+                activeScooterTime.visibility = View.INVISIBLE
+                activeScooterButtonPause.visibility = View.INVISIBLE
+                activeScooterButtonEnd.visibility= View.INVISIBLE
+                scooterFragmentProgressBar.visibility = View.INVISIBLE
+
+            } else {
+                activeScooterTime.stop()
+                activeScooterButtonEnd.isEnabled = false
+                activeScooterButtonPause.isEnabled = false
+                activeScooterName.text = vm.getUserScooter()!!.name
+                activeScooterPrice.text = "100 DKK"
+
+
+
+            }
+        }
+
         return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
 
         with (binding) {
+            activeScooterUnlock.setOnClickListener {
+                vm.getUserScooter()!!.startRideTime = activeScooterTime
+                vm.getUserScooter()?.startRideTime?.start()
+                vm.getUserScooter()?.reserved = true
+                activeScooterUnlock.isEnabled = false
+                activeScooterButtonEnd.isEnabled = true
+                activeScooterButtonPause.isEnabled = true
+            }
 
+            activeScooterButtonPause.setOnClickListener {
+                vm.getUserScooter()?.startRideTime?.stop()
+            }
+
+            activeScooterButtonEnd.setOnClickListener {
+                vm.getUserScooter()?.startRideTime?.stop()
+            }
         }
     }
 
+
 }
+
