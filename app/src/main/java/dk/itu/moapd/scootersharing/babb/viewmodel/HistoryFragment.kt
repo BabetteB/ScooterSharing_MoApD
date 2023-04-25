e7c0ca48bc71e14c66f6bf845fb11b93310a3235
Babette Bækgaard <38117208+BabetteB@users.noreply.github.com>
2023-04-25 10:16:01 +0200

    added recyclerview in history tab for previous rides, added custom adapter has 1 test item in DB - needs to implement such that when finishing a ride add to history in db

--- app/src/main/java/dk/itu/moapd/scootersharing/babb/viewmodel/HistoryFragment.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/babb/viewmodel/HistoryFragment.kt
@@ -1,60 +1,71 @@
 package dk.itu.moapd.scootersharing.babb.viewmodel
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 import dk.itu.moapd.scootersharing.babb.R
+import dk.itu.moapd.scootersharing.babb.RideListFragment
+import dk.itu.moapd.scootersharing.babb.databinding.FragmentHistoryBinding
+import dk.itu.moapd.scootersharing.babb.databinding.FragmentRideListBinding
+import dk.itu.moapd.scootersharing.babb.model.CustomAdapter
+import dk.itu.moapd.scootersharing.babb.model.HistoryAdapter
+import dk.itu.moapd.scootersharing.babb.model.Scooter
+import dk.itu.moapd.scootersharing.babb.model.ScooterViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HistoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HistoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val vm : ScooterViewModel by activityViewModels()
+
+    private lateinit var database: DatabaseReference
+    private lateinit var auth : FirebaseAuth
+
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding
+        get() = checkNotNull(_binding){
+            "Cannot access binding."
+        }
+
+    companion object{
+        const val TAG = "HistoryFragment"
+        private lateinit var adapter : HistoryAdapter
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        Log.d(TAG, "Instance created.")
+
+        database = vm.getDB()
+        auth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        binding.historyRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        val query = database
+            .child("history")
+
+        val options =
+            FirebaseRecyclerOptions.Builder<Scooter>()
+                .setQuery(query, Scooter::class.java)
+                .setLifecycleOwner(this)
+                .build()
+
+        adapter = HistoryAdapter(options)
+        binding.historyRecyclerView.adapter = adapter
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HistoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HistoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
